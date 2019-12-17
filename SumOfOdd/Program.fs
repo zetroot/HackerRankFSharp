364a07614b1791928a002d0fa6e0fa43016384c6
zetroot <zetroot@gmail.com>
2019-12-17 23:46:56 +0300

    2 tasks

---
+++ SumOfOdd/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+(*USER CODE BEGIN*)
+let f arr = 
+    List.filter (fun x -> match x % 2 with | 0 -> false | _ -> true) arr
+    |> List.reduce (+)
+(*USER CODE END*)
+
+let read_and_parse()=
+    let mutable arr = []
+    let mutable buff = Console.ReadLine()
+    while buff <> null do
+            arr <- Int32.Parse(buff)::arr
+            buff <- Console.ReadLine()
+    arr
+
+let main =    
+    let arr = read_and_parse()
+    printf "%A" <| f arr
