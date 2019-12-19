f08313e8f2e6f5fae5b60c12bbf372586f2d63c4
zetroot <zetroot@gmail.com>
2019-12-20 00:09:23 +0300

    polygons solved

---
+++ PolygonArea/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let buildTuple arr =
+    let a = Array.head arr
+    let b = Array.tail arr |> Array.head
+    (a, b)
+
+let calcSeqMember (a:(float*float)) (b:(float*float)) =
+    ((fst a)*(snd b) - (snd a)*(fst b))/2.0
+
+[<EntryPoint>]
+let main argv =
+    let N = Console.ReadLine() |> int
+    let points = 
+        Seq.init N (fun _ -> Console.ReadLine().Split(' ') |> Array.map (fun x -> x |> float)) 
+        |> Seq.map buildTuple
+        |> Seq.toList
+    let pointsBiased = 
+        let newTail = List.init 1 (fun _ -> List.head points)
+        List.append (List.tail points) (newTail)
+    List.map2 calcSeqMember pointsBiased points |> List.reduce (+) |> Math.Abs |> printfn "%f"
+    0 // return an integer exit code
---
+++ PolygonPerimeter/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let buildTuple arr =
+    let a = Array.head arr
+    let b = Array.tail arr |> Array.head
+    (a, b)
+
+let calcDistance (a:(double*double)) (b:(double*double)) =
+    let dx2 = Math.Pow((fst a) - (fst b), 2.0)
+    let dy2 = Math.Pow((snd a) - (snd b), 2.0)
+    Math.Sqrt(dx2 + dy2)
+
+[<EntryPoint>]
+let main argv =
+    let N = Console.ReadLine() |> int
+    let points = 
+        Seq.init N (fun _ -> Console.ReadLine().Split(' ') |> Array.map (fun x -> x |> double)) 
+        |> Seq.map buildTuple
+        |> Seq.toList
+    let pointsBiased = 
+        let newTail = List.init 1 (fun _ -> List.head points)
+        List.append (List.tail points) (newTail)
+    List.map2 calcDistance pointsBiased points |> List.reduce (+) |> printfn "%f"
+    0 // return an integer exit code
