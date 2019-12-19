e9f35fbca0c263aa03ec26bbe6fdd36ce663a6c7
a.sashurin <a.sashurin@amplitech.ru>
2019-12-19 17:51:23 +0300

    funcs or not wip

---
+++ FunctionsOrNot/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let hasDoubledTuples (tupleList:list<int*int>) = 
+    tupleList 
+    |> List.map fst 
+    |> List.countBy (fun x -> x) 
+    |> List.map (fun x -> match snd x with | 0 | 1 -> false | _ -> true)
+    |> List.reduce (fun x y -> x || y)
+
+
+[<EntryPoint>]
+let main argv =
+    let T = Console.ReadLine() |> int
+    let N = Console.ReadLine() |> int
+    let tuplesArr =
+        Seq.init N (fun _ -> 
+            let doubledArr = Console.ReadLine().Split(' ') |> Array.map int
+            let a = Array.head doubledArr
+            let b = Array.skip 1 doubledArr |> Array.head
+            (a, b)
+            )
+        |> Seq.toList
+
+    0 // return an integer exit code
