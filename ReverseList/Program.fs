01aa8ef57b66f856aba59eaaa9153739a9ea10aa
a.sashurin <a.sashurin@amplitech.ru>
2019-12-19 14:44:28 +0300

    reverse list

---
+++ ReverseList/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let elemToList x =
+    List.init 1 (fun _ -> x)
+
+let rec reverse x =
+    match x with
+    | list when List.length list > 1 -> List.head x |> elemToList |> List.append (List.tail x |> reverse)
+    |_ -> x
+
+[<EntryPoint>]
+let main argv =
+    let list =
+        Seq.initInfinite(fun _ -> Console.ReadLine())
+        |> Seq.takeWhile(String.IsNullOrWhiteSpace >> not)
+        |> Seq.toList
+    
+    reverse list |> List.iter (printfn "%s")    
+    0 // return an integer exit code
