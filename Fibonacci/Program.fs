5dddd7888acbf54c60c132cf4003d2ad1432b0c2
zetroot <zetroot@gmail.com>
2019-12-23 01:11:32 +0300

    2 recursive tasks

---
+++ Fibonacci/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec fib n =
+    match n with
+    | 1 -> 0
+    | 2 -> 1
+    | x -> fib (x-1) + fib (x-2)
+
+[<EntryPoint>]
+let main argv =
+    let n = Console.ReadLine() |> int
+    let result = fib n
+    printfn "%d" result
+    0 // return an integer exit code
---
+++ GCD_Euclid/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec gcd (x:int) (y:int) = 
+    match x = y with
+    | true -> x
+    | false -> gcd (Math.Max(x, y) - Math.Min(x,y)) (Math.Min(x, y))
+
+[<EntryPoint>]
+let main argv =
+    let input = Console.ReadLine().Split(" ") |> Array.map int
+    let res = gcd input.[0] input.[1]
+    printfn "%d" res
+    0 // return an integer exit code
